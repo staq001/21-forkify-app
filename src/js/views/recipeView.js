8924aac21ca77fd1f8f3948822c057ecd85bf7bc
staq001 <dame.hush@gmail.com>
2022-09-07 14:11:34 +0100

    New stuff

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -125,7 +125,8 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        // ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? ing.quantity : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
